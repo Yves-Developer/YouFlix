02e2cc42ce80a5f8d226cf30761247339bf05cf0
Yves-Developer <yvesmugisha09@gmail.com>
2025-04-05 20:40:04 +0200

    # search spacing

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 const Searchbar = () => {
   return (
-    <View className="flex-row items-center gap-2 mt-16 bg-secondary px-4">
+    <View className="flex-row items-center gap-2 mt-16 bg-secondary px-4 py-2">
       <Ionicons name="search-sharp" size={24} color="#a8b5db" />
       <TextInput
         placeholder="Search"
